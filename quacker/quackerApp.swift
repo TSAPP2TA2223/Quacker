d30ce5d56725dea567ea217f9116a15bc843ae98
Abby Dominguez <abbytorade@icloud.com>
2022-10-29 00:32:26 +0200

    Initial Commit

---
+++ quacker/quackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  quackerApp.swift
+//  quacker
+//
+//  Created by Abby Dominguez on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct quackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
