a6e662476a1df3ad9a987b1be825d8172e940add
Abby Dominguez <68694729+abbydomi@users.noreply.github.com>
2022-10-30 00:41:57 +0200

    Revert "Feature/login"

--- quacker/quackerApp.swift
+++ quacker/quackerApp.swift
@@ -7,8 +7,13 @@
 
 import SwiftUI
 import FirebaseCore
-import GoogleSignIn
-
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 @main
 struct quackerApp: App {
     // register app delegate for Firebase setup
@@ -19,12 +24,4 @@ struct quackerApp: App {
             SplashScreenView()
         }
     }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-
 }
